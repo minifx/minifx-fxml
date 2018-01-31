2fc862966ba7c39b84a4c94fe1e0b0ba85c19e3b
Kajetan Fuchsberger <Kajetan.Fuchsberger@cern.ch>
2018-01-31 14:44:44 +0000

    rename
    git-svn-id: svn+ssh://svn.cern.ch/reps/acc-co/trunk/accsoft/gui/accsoft-gui-fx-fxmlloading@454427 4e1e14d5-dc27-4203-babc-ce7d9a7dada2

---
+++ src/java/org/minifx/fxmlloading/service/DummyClass.java
@@ -0,0 +1,9 @@
+/**
+ * Copyright (c) 2018 European Organisation for Nuclear Research (CERN), All Rights Reserved.
+ */
+
+package org.minifx.fxmlloading.service;
+
+public class DummyClass {
+
+}
---
+++ src/test/org/minifx/fxmlloading/service/DummyTest.java
@@ -0,0 +1,18 @@
+/**
+ * Copyright (c) 2018 European Organisation for Nuclear Research (CERN), All Rights Reserved.
+ */
+
+package org.minifx.fxmlloading.service;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DummyTest {
+
+    @Test
+    public void test() {
+        fail("Not yet implemented");
+    }
+
+}
