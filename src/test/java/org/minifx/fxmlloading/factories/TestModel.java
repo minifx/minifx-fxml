ca1df19b4deef24e5423585571fde34bace78d84
acalia <andrea.calia@cern.ch>
2019-02-19 16:03:37 +0100

    ported to standard maven structure

---
+++ src/test/java/org/minifx/fxmlloading/factories/TestModel.java
@@ -0,0 +1,15 @@
+/**
+ * Copyright (c) 2017 European Organisation for Nuclear Research (CERN), All Rights Reserved.
+ */
+
+package org.minifx.fxmlloading.factories;
+
+import javax.inject.Inject;
+
+import javafx.geometry.Side;
+
+public class TestModel {
+    
+    @Inject
+    Side toolBarSide;
+}
---
+++ src/test/java/org/minifx/fxmlloading/factories/TestService.java
@@ -0,0 +1,17 @@
+/**
+ * Copyright (c) 2017 European Organisation for Nuclear Research (CERN), All Rights Reserved.
+ */
+
+package org.minifx.fxmlloading.factories;
+
+import javax.inject.Inject;
+
+public class TestService {
+
+    @Inject
+    TestModel model;
+    
+    TestService() {
+        // private constructor
+    }
+}
